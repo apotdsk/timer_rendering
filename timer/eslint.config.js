13e2d7dbda1c7b3e72beb8e08de63c88dae0aed8
apotapov <andriyp22sk@gmail.com>
2025-12-17 19:58:23 +0200

    structure

--- timer/eslint.config.js
+++ timer/eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -19,5 +20,19 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
-])
+]);
---
+++ timer/src/components/Timer/Timer.tsx
@@ -0,0 +1,81 @@
+import styles from './Timer.module.scss';
+import { useEffect, useState } from 'react';
+
+export const Timer = () => {
+  const [time, setTime] = useState(new Date());
+  const [isActive, setActive] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const stopTimer = () => {
+    setActive(false);
+  };
+
+  const startTimer = () => {
+    setActive(true);
+  };
+
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.main_time}>
+        {String(time.getMinutes()).padStart(2, '0')}:
+        {String(time.getSeconds()).padStart(2, '0')}
+      </h1>
+      <p className={styles.main_renders}>Number of Component Renders: X</p>
+      <div className={styles.main_divider}></div>
+      <div className={styles.main_buttons}>
+        <button
+          className={`${styles.button_pause} ${styles.btn} ${isActive ? '' : styles.btn_hidden}`}
+        >
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6 3H5C4.73478 3 4.48043 3.10536 4.29289 3.29289C4.10536 3.48043 4 3.73478 4 4V12C4 12.2652 4.10536 12.5196 4.29289 12.7071C4.48043 12.8946 4.73478 13 5 13H6C6.26522 13 6.51957 12.8946 6.70711 12.7071C6.89464 12.5196 7 12.2652 7 12V4C7 3.73478 6.89464 3.48043 6.70711 3.29289C6.51957 3.10536 6.26522 3 6 3ZM11 3H10C9.73478 3 9.48043 3.10536 9.29289 3.29289C9.10536 3.48043 9 3.73478 9 4V12C9 12.2652 9.10536 12.5196 9.29289 12.7071C9.48043 12.8946 9.73478 13 10 13H11C11.2652 13 11.5196 12.8946 11.7071 12.7071C11.8946 12.5196 12 12.2652 12 12V4C12 3.73478 11.8946 3.48043 11.7071 3.29289C11.5196 3.10536 11.2652 3 11 3Z"
+              fill="white"
+            />
+          </svg>
+          &nbsp;Pause
+        </button>
+        <button
+          className={`${styles.button_reset} ${styles.btn} ${isActive ? '' : styles.btn_hidden}`}
+        >
+          Reset
+        </button>
+        <button
+          className={`${styles.button_play} ${styles.btn} ${isActive ? styles.btn_hidden : ''}`}
+        >
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4.604 3.02467C4.54327 2.98866 4.47409 2.96937 4.40349 2.96875C4.33289 2.96813 4.26339 2.98621 4.20204 3.02115C4.14069 3.0561 4.08968 3.10665 4.0542 3.16769C4.01871 3.22872 4.00001 3.29806 4 3.36867V12.6313C4.00001 12.7019 4.01871 12.7713 4.0542 12.8323C4.08968 12.8933 4.14069 12.9439 4.20204 12.9788C4.26339 13.0138 4.33289 13.0319 4.40349 13.0312C4.47409 13.0306 4.54327 13.0113 4.604 12.9753L12.4193 8.344C12.479 8.30858 12.5285 8.25824 12.5628 8.19793C12.5972 8.13762 12.6152 8.0694 12.6152 8C12.6152 7.93059 12.5972 7.86238 12.5628 7.80207C12.5285 7.74176 12.479 7.69142 12.4193 7.656L4.604 3.02467Z"
+              fill="white"
+              stroke="white"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          &nbsp;Play
+        </button>
+      </div>
+    </main>
+  );
+};
